0c01dab5cc8f7c6d88e4659eac0edd4377949189
webdevsha <43009213+webdevsha@users.noreply.github.com>
2025-12-20 16:46:47 +0800

    Delete permit-akaun directory

--- permit-akaun/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/login-form"
-
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-secondary/30">
-      <LoginForm />
-    </div>
-  )
-}
